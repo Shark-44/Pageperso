a181efb066fea612ccbb5b59f2c05c8e1372d21a
Shark-44 <joanny.bernardeau@gmail.com>
2023-12-13 21:09:29 +0100

    vu les problemes avec Vercel et github. C'est que cela n'a pas pris en compte les majuscules de deux planetes. Supprime Saturne et Mercure

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -6,8 +6,7 @@ import Jupiter from "../assets/space/Jupiter.webp"
 import Mars from "../assets/space/Mars.webp"
 import Venus from "../assets/space/Venus.webp"
 import Terre from "../assets/space/Terre.webp"
-import Mercure from "../assets/space/Mercure.webp"
-import Saturne from "../assets/space/Saturne.webp"
+
 
 
 const SolarSystem = ({vitesse}) => {
@@ -15,12 +14,12 @@ const SolarSystem = ({vitesse}) => {
   return (
     <div className="solar-system">
       <Sun />
-      <Planet name="Mercure" orbitRadius={50} rotationSpeed={1 * vitesse} urlimg={Mercure} />
+      <Planet name="Mercure" orbitRadius={50} rotationSpeed={1 * vitesse} urlimg={Terre} />
       <Planet name="Venus" orbitRadius={100} rotationSpeed={1.6 * vitesse} urlimg={Venus}/>
       <Planet name="Terre" orbitRadius={150} rotationSpeed={4 * vitesse} urlimg={Terre}/>
       <Planet name="Mars" orbitRadius={200} rotationSpeed={8 * vitesse} urlimg={Mars}/>
       <Planet name="Jupiter" orbitRadius={250} rotationSpeed={48 * vitesse} urlimg={Jupiter}/>
-      <Planet name="Saturne" orbitRadius={300} rotationSpeed={120 * vitesse} urlimg={Saturne}/>        
+      <Planet name="Saturne" orbitRadius={300} rotationSpeed={120 * vitesse} urlimg={Terre}/>        
     </div>
   );
 };
