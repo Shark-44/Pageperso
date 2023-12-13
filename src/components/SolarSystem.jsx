c86a78678d2e8f3a99bc7e24ec55b3a4cb601708
Shark-44 <joanny.bernardeau@gmail.com>
2023-12-13 15:09:42 +0100

    encore des essais

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types'
 import Sun from './Sun'
 import Planet from './Plantes'
 import "./SolarSystem.css"
-import Jupiter from "../assets/space/Jupiter.webp"
-import Mercure from "../assets/space/Mercure.webp"
-import Saturne from "../assets/space/Saturne.webp"
-import Terre from "../assets/space/Terre.webp"
-import Venus from "../assets/space/Venus.webp"
-import Marsp from "../assets/space/Marsp.webp"
+import Jupiter from "../assets/space/Jupiter.webp";
+import Mercure from "../assets/space/Mercure.webp";
+import Saturne from "../assets/space/Saturne.webp";
+import Terre from "../assets/space/Terre.webp";
+import Venus from "../assets/space/Venus.webp";
+import Mars from "../assets/space/Mars.webp";
 
 const SolarSystem = ({vitesse}) => {
   
@@ -17,7 +17,7 @@ const SolarSystem = ({vitesse}) => {
       <Planet name="Mercure" orbitRadius={50} rotationSpeed={1 * vitesse} urlimg={Mercure} />
       <Planet name="Venus" orbitRadius={100} rotationSpeed={1.6 * vitesse} urlimg={Venus}/>
       <Planet name="Terre" orbitRadius={150} rotationSpeed={4 * vitesse} urlimg={Terre}/>
-      <Planet name="Mars" orbitRadius={200} rotationSpeed={8 * vitesse} urlimg={Marsp}/>
+      <Planet name="Mars" orbitRadius={200} rotationSpeed={8 * vitesse} urlimg={Mars}/>
       <Planet name="Jupiter" orbitRadius={250} rotationSpeed={48 * vitesse} urlimg={Jupiter}/>
       <Planet name="Saturne" orbitRadius={300} rotationSpeed={120 * vitesse} urlimg={Saturne}/>        
     </div>
