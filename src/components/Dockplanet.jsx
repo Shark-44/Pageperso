354035272a6144228096d2b4cf5a5a258d3f904f
Shark-44 <joanny.bernardeau@gmail.com>
2023-12-13 14:22:11 +0100

    erreur signalé par Vercel corrigée

--- src/components/Dockplanet.jsx
+++ src/components/Dockplanet.jsx
@@ -1,6 +1,6 @@
 import "./Dockplanet.css"
 import Jupiter from "../assets/space/Jupiter.webp"
-import Mars from "../assets/space/Mars.webp"
+import Mars from "../assets/space/Marsp.webp"
 import Mercure from "../assets/space/Mercure.webp"
 import Saturne from "../assets/space/Saturne.webp"
 import Terre from "../assets/space/Terre.webp"
