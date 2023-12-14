34aa9ec4ca7d228b0a2266f728e0477490a32dcf
Shark-44 <joanny.bernardeau@gmail.com>
2023-12-14 13:01:14 +0100

    premier jet du retour d'info planetes

--- src/pages/SystemSolar.jsx
+++ src/pages/SystemSolar.jsx
@@ -6,10 +6,12 @@ import SolarSystem from "../components/SolarSystem"
 import Horloge from "../components/Horloge"
 import Accelerator from "../components/Accelerator"
 import Dockplanet from '../components/Dockplanet'
+import Infoplant from './Infoplant'
 
 function SystemSolar () {
     const vitesse =[20, 10, 1, 0.5, 0.3]
     const [currentIndex, setCurrentIndex] = useState(2)
+    const [infoIndex, setInfoIndex] = useState()
 
     return (
         <div className="contenairSolar">
@@ -20,11 +22,14 @@ function SystemSolar () {
             <SolarSystem vitesse={vitesse[currentIndex]}/>
             <div className="infoplanets">
                 <Horloge />
+                <div className="panel">
+                    <Infoplant infoIndex={infoIndex} />
+                </div>
             </div>
             <h4>Choisissez et cliquez</h4>
             <LiaHandPointRightSolid style={{position: 'absolute', bottom: '20px', left: '27%', color: 'white'}} />
             <div className="dock">
-                <Dockplanet />
+                <Dockplanet setInfoIndex={setInfoIndex} />
             </div>
         </div>
     )
