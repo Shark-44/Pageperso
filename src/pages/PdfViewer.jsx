eb0f26599262f8bfc4e9c7cdaa45cca8d92ab291
Shark-44 <joanny.bernardeau@gmail.com>
2024-01-16 14:04:18 +0100

    test por pdf vercel

--- src/pages/PdfViewer.jsx
+++ src/pages/PdfViewer.jsx
@@ -2,10 +2,10 @@
 // import PropTypes from 'prop-types';
 import "./PdfViewer.css"
 import { Viewer, Worker} from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css'
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 
-import mypdf from "../PDF/CVJoanny.pdf"
+import mypdf from "./CVJoanny.pdf"
 import '../styles/index.css'
 
 
@@ -17,7 +17,9 @@ const PdfViewer = () => {
     <div>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
       <div id="pdfviewer">
-        <Viewer fileUrl={mypdf} /> 
+      <Viewer
+    fileUrl={mypdf} />
+
     </div>
       </Worker>
         </div>
