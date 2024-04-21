d1ac857a7946aef0378320ab6e592a712639ed35
Shark-44 <joanny.bernardeau@gmail.com>
2024-04-21 15:11:36 +0200

    rendu de gsap

--- src/pages/Nature.jsx
+++ src/pages/Nature.jsx
@@ -4,66 +4,79 @@ import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-
 gsap.registerPlugin(ScrollTrigger);
 
 function Nature() {
     function changeH4Content(boxIndex) {
-        console.log(boxIndex)
+        console.log(boxIndex);
         let newText = "";
         switch (boxIndex) {
-          case 1:
-            newText = "Dauphin";
-            break;
-          case 2:
-            newText = "Tortue";
-            break;
-          case 3:
-            newText = "Poisson";
-            break;
-          default:
-            newText = "Dauphin";
+            case 1:
+                newText = "Dauphin";
+                break;
+            case 2:
+                newText = "Tortue";
+                break;
+            case 3:
+                newText = "Poisson";
+                break;
+            default:
+                newText = "Dauphin";
         }
         document.querySelector(".box h4.text").textContent = newText;
-      }
-     
-      useEffect(() => {
+    }
+
+    useEffect(() => {
         changeH4Content(1);
         const boxes = document.querySelectorAll(".box");
         boxes.forEach((box, index) => {
-          box.setAttribute("data-index", index + 1);
-      
-          ScrollTrigger.create({
-            trigger: box,
-            start: "top 40%",
-            onEnter: () => {
-              const boxIndex = parseInt(box.getAttribute("data-index"));
-              changeH4Content(boxIndex);
-            },
-            onLeaveBack: () => {
-              const boxIndex = parseInt(box.getAttribute("data-index"));
-              if (boxIndex !== 1) {
-                changeH4Content(boxIndex - 1);
-              }
-            },
-          });
+            box.setAttribute("data-index", index + 1);
+
+            ScrollTrigger.create({
+                trigger: box,
+                start: "top 40%",
+                onEnter: () => {
+                    const boxIndex = parseInt(box.getAttribute("data-index"));
+                    changeH4Content(boxIndex);
+                },
+                onLeaveBack: () => {
+                    const boxIndex = parseInt(box.getAttribute("data-index"));
+                    if (boxIndex !== 1) {
+                        changeH4Content(boxIndex - 1);
+                    }
+                },
+            });
         });
-      }, []);
-      
-        
-  return (
-    <>
-       <Navbar />
-      <div className="contenairimage">
-    
-        <div className="box b1">
-          <h4 className="text"></h4>
-        </div>
-        <div className="box b2"></div>
-        <div className="box b3"></div>
-      </div>
-    </>
-  );
+
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".contenairtest",
+                start: "top bottom",
+                toggleActions: "play none none reverse",
+            },
+        });
+
+        tl.to(".text", { opacity: 0, duration: 0.2 });
+    }, []);
+
+    return (
+        <>
+            <Navbar />
+            <div className="contenairimage">
+                <div className="box b1">
+                    <h4 className="text"></h4>
+                </div>
+                <div className="box b2"></div>
+                <div className="box b3"></div>
+            </div>
+            <div className="contenairtest">
+                <h2>Cette page</h2>
+                <p>Comme la page <a href="https://www.apple.com/fr/apple-tv-4k/" target="_blank" rel="noreferrer" >Apple TV 4K </a>, j&apos;ai voulu travailler sur une section a travers GSAP Scrolltrigger. Je n&apos;ai pas enclenché de vidéo mais joué sur des textes.<br />
+                Ainsi de comprendre des mécanismes et continuer a développer  </p>
+            </div>
+        </>
+    );
 }
 
 export default Nature;
