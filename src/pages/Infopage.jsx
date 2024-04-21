fb5439268173d2763de49a4f4169a5e8f12323a1
Shark-44 <joanny.bernardeau@gmail.com>
2024-04-21 15:45:47 +0200

    rendu de gsap

--- src/pages/Infopage.jsx
+++ src/pages/Infopage.jsx
@@ -3,22 +3,21 @@ import "./Infopage.css"
 
 function Infopage() {
     return (
+        <>
+         <Navbar />
         <div className="contenairInfo">
-        <Navbar />
-        <div className="text">
+       
+        
         <h2>Pourquoi cette ce site</h2>
-        <p>Nous sommes début décembre 2023. le pourquoi ... <br /> Je devais me faire un portefolio, montrer ce dont je suis capable. Me faire remarquer par une personne curieuse et désireuse d&apos;en savoir plus sur moi.  <br />L&apos;idée était donc de savoir ce que j&apos;allais y mettre, mais aussi avoir une feuille de route. Montrer que j&apos;évolue,
-        avoir l&apos;esprit Dev. <br />
-        Mais qu&apos;est-ce que l&apos;esprit Dev? Si en premier lieu un développeur est une personne capable de créer une API pour un client, certains auront une approche visuelle et d&apos;autres se concentreront sur les techniques à mettre en place. Je me situe davantage dans cette seconde catégorie, tout en souhaitant également obtenir un rendu final avec un aspect waouh! <br />
-        C&apos;est également dans cet aspect technique que j&apos;adore évoluer, ce qui a été source de motivation tout au long de ma carrière professionnelle. C&apos;est un peu comme jouer aux echecs avec quelques coups d&apos;avance. <br />
-        Pour moi, l&apos;esprit Dev, c&apos;est penser à un résultat final, composé de challanges. Je ne veux pas copier-coller le travail d&apos;un autre. A moins que je ne sois sûr de le maitrisser et le surpasser! Derrière cela, il y a aussi la force d&apos;un Dev. Apporter une réponse à un couac! <br />
-        Le métier de dev peut sembler redondant et c&apos;est là que la curiosité peut le pousser a relever des défis, ne pas se cantonner à faire la même chose. c&apos;est pour moi une perspective personnelle: grandir, explorer et devenir meilleur ! </p>
+        <p>Nous sommes début décembre 2023. Après mon examen de DWWM, je devais me faire un portfolio.<br /> Mais l&apos;idée de créer une page perso pour continuer à développer m&apos;inspirait plus<br />
+        Oui il y a tellement de sujet a parcourir. Je voulais avoir une trace, pouvoir revenir sur des points, montrer une évolution  </p>
         <h2> Ma page perso</h2>
         <p>Je souhaitais commencer par une introduction avec une vidéo me caractérisant. Oui j&apos;apprécie la tranquilité, le calme et la nature. Commme tout le monde, j&apos;ai parfois le besoin de décompresser et évacuer le stress. J&apos;ai pour projet une transition à ma page d&apos;accueil. Bien que cela reste pour le moment en suspens. <br /> 
             Ensuite je voulais créer des pages avec des effets sans copier et coller d&apos;un quelconque code. Certes mon projet de DWWM ou j&apos;ai passé des heures à comprendre des concepts, découvrir le backend m&apos;aurait plus. Cependant il me fallait une pause sur ce sujet, et je voulais explorer d&apos;autres domaines. M&apos;initier au PHP, oui mais c&apos;est aussi une manière de reviser des principes que je continue à mettre en pratique. C&apos;est aussi une manière d&apos;entretenir des connaissances. Mais il y a tellement de sujets que je veux explorer, Framer motion, la 3D... <br />Chaque jours, même le dimanche je m&apos;immerge dans cette curiosité.<br /> Bon je tiens à rassurer, je sais lâcher le clavier et l&apos;ecran.<br /> Alors l&apos;idée du système solaire m&apos;est venue. Je suis parti de l&apos;idée de céer une ellipse sous React avec des composants que j&apos;ajouterai au fil du temps. Découvrir comment deployer, comprendre de nouveaux problèmes ( Faut ce le dire, quand cela marche en local, cela ne fonctionne pas forcement sur un serveur, Grrrr!) <br />
         Dans mes recherches, j&apos;ai découvert une personne qui avait eu la même idée, mais dans un domaine plus avancé avec la 3D et canvas. Cependant je souhaite apprendre de son code, après avoir finalisé mon projet. Il y a un site que je trouve génial <a href="https://www.solarsystemscope.com/skydiary/" target="_blank" rel="noreferrer" >Solarsystemscope</a>. Mais j&apos;en suis a des années lumieres. Alors comprendre un exemple simple, travailler dessus, apporter un sens, une direction avec un effet ludique. Voilà mon premier objectif</p>
         </div>
-        </div>)
+        </>
+    )
 
 }
 export default Infopage
