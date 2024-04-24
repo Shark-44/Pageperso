021cecf70913674cf28a0b687277bcdb2bc206ff
Shark-44 <joanny.bernardeau@gmail.com>
2024-04-24 13:18:51 +0200

    essais de transition

--- src/components/Transvideo.jsx
+++ src/components/Transvideo.jsx
@@ -13,7 +13,7 @@ function Transvideo () {
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
 
-        gsap.set(".transt1", { scale: 0 });
+        gsap.set(".transt1", { scale: 1});
         gsap.set(".transt2", { y: 0 });
 
         const tl = gsap.timeline({
@@ -33,7 +33,7 @@ function Transvideo () {
         tl.to(".transt2", {y: -725});
 
     }, []);
-    
+
     return (
         <div className="transition" ref={containerTRef} >
             <div className="transt1">
